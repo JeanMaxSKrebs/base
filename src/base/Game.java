1af8bf0be5a080ec5974ffe19651fb62c68439cd
Jean Max S Krebs <jeanmaxskrebs@gmail.com>
2021-04-21 15:47:42 -0300

    Game modificado

--- src/base/Game.java
+++ src/base/Game.java
@@ -3,31 +3,77 @@ package base;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import javax.swing.JFrame;
 
-@SuppressWarnings("serial")
-public class Game extends Canvas implements Runnable{
+import entities.Enemy;
+import entities.Entity;
+import entities.Player;
+import entities.Power;
+import graficos.Spritesheet;
+import graficos.UI;
+import world.Tiledoor;
+import world.World;
 
+public class Game extends Canvas implements Runnable, KeyListener{
+	
+	private static final long serialVersionUID = 1L;
 	private static JFrame frame;
 	private Thread thread;
 	private boolean isRunning;
-
-	private final int WIDTH = 160;
-	private final int HEIGHT = 120;
-	private final int SCALE = 3;
 	
+	public static int maximumCritic = 100;
+	public static int maximumDodge = 50;
+	
+	public static final int WIDTH = 320;
+	public static final int HEIGHT = 320;
+	public static final int SCALE = 2;
+
 	private BufferedImage image;
+	
+	public static List<Entity> entities;
+	public static List<Enemy> enemies;
+	public static List<Tiledoor> tiledoors;
+	public static List<Power> powers;	
+	
+	public static Spritesheet spritesheet;
+	
+	public static World world;
+	
+	public static Player player;
+	
+	public UI ui;
+	
+	public static String gameState = "NORMAL"; 
 		
 	public Game() {
+		addKeyListener(this);
 		this.setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
 		
 		initFrame();
-
+		ui = new UI();
 		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
-	
+		
+		powers = new ArrayList<Power>();
+		tiledoors = new ArrayList<Tiledoor>();
+		entities = new ArrayList<Entity>();
+		enemies = new ArrayList<Enemy>();
+		enemies = new ArrayList<Enemy>();
+		spritesheet = new Spritesheet("/spritesheet.png");
+		player = new Player(0, 0, 32, 32, spritesheet.getSprite(0, 32, 32, 32));
+		world = new World("/map.png");
+		entities.add(player);
+		
 	}
 
 	public void initFrame() {
@@ -61,8 +107,20 @@ public class Game extends Canvas implements Runnable{
 	}
 	
 	public void tick() {
-		
-//		System.out.println("Tick");
+		if(gameState == "NORMAL") {
+			
+			for(int i=0; i<entities.size(); i++) {
+				Entity e = entities.get(i);
+				e.tick();
+			}
+			for (int i = 0; i < powers.size(); i++) {
+				powers.get(i).tick();
+			}
+		} else if(gameState == "GAME OVER") {
+			
+		} else if(gameState == "WIN") {
+			
+		}
 	}
 	
 	public void render() {
@@ -72,10 +130,49 @@ public class Game extends Canvas implements Runnable{
 			return;
 		}
 		Graphics g = image.getGraphics();
-		g.setColor(new Color(100,100,100));
+		g.setColor(new Color(0, 0, 0));
 		g.fillRect(0, 0, WIDTH, HEIGHT);
+		
+		
+//		Graphics2D g2 = (Graphics2D) g;
+		//render mundo
+		world.render(g);
+		//render entidades
+		for(int i=0; i<entities.size(); i++) {
+			Entity e = entities.get(i);
+			e.render(g);
+		}
+		//render power
+		for (int i = 0; i < powers.size(); i++) {
+			powers.get(i).render(g);
+		}
+		
+		ui.render(g);
+		
+		
+		g.dispose();
 		g = bs.getDrawGraphics();
 		g.drawImage(image, 0, 0, WIDTH*SCALE, HEIGHT*SCALE, null);
+		
+		if(gameState=="GAME_OVER") {
+			Graphics2D g2 = (Graphics2D) g;
+			g2.setColor(new Color(0, 0, 0, 100));
+			g2.fillRect(0, 0, WIDTH*SCALE, HEIGHT*SCALE);
+			g.setFont(new Font("Arial", Font.BOLD, 20));
+			g.fillRect(0, 0, WIDTH*SCALE, HEIGHT*SCALE);
+			g.setColor(Color.white);
+			g.drawString("GAME OVER",  WIDTH/2, HEIGHT/2);
+		}
+		if(gameState=="WIN") {
+			Graphics2D g2 = (Graphics2D) g;
+			g2.setColor(Color.GRAY);
+			g2.fillRect(0, 0, WIDTH*SCALE, HEIGHT*SCALE);
+			g.setFont(new Font("Arial", Font.BOLD, 20));
+			g.fillRect(0, 0, WIDTH*SCALE, HEIGHT*SCALE);
+			g.setColor(Color.white);
+			g.drawString("Parabéns  "+"Você pegou: "+Game.player.stamine+"StamineBags",  WIDTH/2, HEIGHT/2);
+		}
+		
 		bs.show();
 	}
 	
@@ -86,6 +183,7 @@ public class Game extends Canvas implements Runnable{
 		double delta = 0;
 		int frames = 0;
 		double timer = System.currentTimeMillis();
+		requestFocus();
 		System.out.println("Jogo Rodando...");
 		while(isRunning) {
 			long now = System.nanoTime();
@@ -105,5 +203,49 @@ public class Game extends Canvas implements Runnable{
 			}
 		}
 		stop();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+	public void keyPressed(KeyEvent e) {
+
+		if(e.getKeyCode() == KeyEvent.VK_UP||e.getKeyCode() == KeyEvent.VK_W) {
+			player.up = true;
+		} else if(e.getKeyCode() == KeyEvent.VK_DOWN||e.getKeyCode() == KeyEvent.VK_S){
+			player.down = true;
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT||e.getKeyCode() == KeyEvent.VK_D) {
+			player.right = true;
+		} else if(e.getKeyCode() == KeyEvent.VK_LEFT||e.getKeyCode() == KeyEvent.VK_A) {
+			player.left = true;
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_ENTER||e.getKeyCode() == KeyEvent.VK_SPACE) {
+			player.usingPower = true;
+		}
+	}
+
+ 	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_UP||e.getKeyCode() == KeyEvent.VK_W) {
+			player.up = false;
+		} else if(e.getKeyCode() == KeyEvent.VK_DOWN){
+			player.down = false;
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT||e.getKeyCode() == KeyEvent.VK_D) {
+			player.right = false;
+		} else if(e.getKeyCode() == KeyEvent.VK_LEFT||e.getKeyCode() == KeyEvent.VK_A) {
+			player.left = false;
+		}
+	}
+ 	
+	public static int random(int value) {
+		Random random = new Random();
+		int r = random.nextInt(value);
+		return r;
 	}
 }
