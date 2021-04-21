2c02c88b950d5b9a978f19b5ce81f8e47bebc485
Jean <jeanmaxskrebs@gmail.com>
2021-04-21 19:01:11 -0300

    Iniciando specialthings e Level 3 adicionado

--- src/base/Game.java
+++ src/base/Game.java
@@ -39,7 +39,7 @@ public class Game extends Canvas implements Runnable, KeyListener{
 	public static final int HEIGHT = 320;
 	public static final int SCALE = 2;
 
-	private int CUR_LEVEL = 1, MAX_LEVEL = 2;
+	private int CUR_LEVEL = 1, MAX_LEVEL = 3;
 
 	private BufferedImage image;
 	
